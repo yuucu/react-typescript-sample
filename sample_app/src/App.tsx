45ad2fc59c670bcc89d716827edd2b286892cb4d
yuucu <yuucu.sub@gmail.com>
2020-07-29 12:52:04 -0400

    sample material-ui

--- sample_app/src/App.tsx
+++ sample_app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>aiueo</p>
+        <Button variant="outlined">Hello World</Button>
       </header>
     </div>
   );
