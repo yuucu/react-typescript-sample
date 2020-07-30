72a65bb909d20946df06912b637438c630b59b68
yuucu <yuucu.sub@gmail.com>
2020-07-30 13:18:59 -0400

    react practice

--- sample_app/src/App.tsx
+++ sample_app/src/App.tsx
@@ -1,27 +1,59 @@
 import React from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import { Typography, TextField, Button, CssBaseline, Container, DialogTitle, Dialog } from '@material-ui/core';
+
+type HelloDialogProps = {
+open: boolean;
+name: string;
+onClose: () => void;
+}
+
+function HelloDialog(props: HelloDialogProps) {
+  const { open, name, onClose } = props;
+
+  return (
+      <Dialog onClose={onClose} open={open}>
+      <DialogTitle>ご挨拶</DialogTitle>
+      <Typography variant="h5">こんにちわ、{name}さん！</Typography>
+      </Dialog>
+      );
+}
 
 
 function App() {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>aiueo</p>
-        <Button variant="outlined">Hello World</Button>
-      </header>
-    </div>
+  <Container maxWidth="sm" >
+    <CssBaseline />
+    <Typography variant="h5">hello</Typography>
+    <form>
+    <TextField
+      label="name"
+      name="name"
+      variant="outlined"
+      size="small"
+      fullWidth
+      margin="normal" />
+    <Button
+      variant="contained"
+      color="primary"
+      fullWidth
+      onClick={handleClickOpen}>
+      Click!
+    </Button>
+    <HelloDialog open={open} name={"TODO"} onClose={handleClose} />
+    </form>
+    </Container>
   );
 }
 
